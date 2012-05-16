8a9b7e9b7d53b65b49cb1506508cbb67b7bf6aa6
jvillarejo <arzivian87@gmail.com>
2012-05-16 19:47:34 -0300

    creado primer test

---
+++ src/test/java/ar/edu/utn/tadp/empresa/CostoTest.java
@@ -0,0 +1,31 @@
+package ar.edu.utn.tadp.empresa;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.edu.utn.tadp.reunion.Reunion;
+import static org.mockito.Mockito.*;
+
+public class CostoTest {
+
+	private Reunion reunion;
+
+	@Before 
+	public void setUp() {
+		reunion = mock(Reunion.class);
+	}
+	
+	@Test
+	public void testCostoDeTransporte() {
+	}
+
+	@Test
+	public void testCostoDeCatering() { 
+		
+	}
+	
+	@Test
+	public void testCostoDeUnaPersona() { 
+		
+	}
+}
