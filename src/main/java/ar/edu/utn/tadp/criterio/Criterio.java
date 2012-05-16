5ad75db4b9afbf15c49d830530d72abc27171d94
Clara Allende <clari.allende@gmail.com>
2012-05-16 15:11:41 -0300

    Bueno, fui por la opción de la sobrecarga. Hay que definir la interfaz con los recursos y hacer los tests.

--- src/main/java/ar/edu/utn/tadp/criterio/Criterio.java
+++ src/main/java/ar/edu/utn/tadp/criterio/Criterio.java
@@ -2,19 +2,43 @@ package ar.edu.utn.tadp.criterio;
 import java.util.Collection;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
+
+import ar.edu.utn.tadp.recurso.Persona;
 import ar.edu.utn.tadp.recurso.Recurso;
 
-public abstract class Criterio {
-	public abstract boolean cumpleCondicion(Recurso unRecurso);
-	
+/** 
+ * Representa un command que sabe filtrar a las personas.
+ *  @author clari
+ *
+ */
+public  class Criterio {
+	/**
+	 * @param 
+	 * una coleccion de recursos
+	 * @return 
+	 * una colección de Iterables con los elementos que cumplen con el criterio
+	 * 
+	 */
 	public Iterable <Recurso> filtrarConjunto(Collection <Recurso> recursos){
-	//template method
-		final Criterio actual=this; 
+		//si, le puse self :P y la tengo que declarar final porque sino se queja ¬¬
+		final Criterio self=this; 
 	    Predicate<Recurso>p= new Predicate<Recurso>() {
 	    	public boolean apply(Recurso unRecurso){
-	    		return actual.cumpleCondicion(unRecurso);
+	    		return self.cumpleCondicion(unRecurso);
 	    	}
 	    };
 	    	return	Iterables.filter(recursos, p);
 	}
+	
+	public boolean cumpleCondicion(Recurso unRecurso){
+		//unRecurso.estaDisponible();
+		return false;
+	}
+	
+	public boolean cumpleCondicion(Persona unaPersona){
+		//double dispatch? 
+		//contains() o elementEquals() de guava
+		//unaPersona.cumpleCondicion(this);
+		return false;
+		}
 }
