aa5ca69ff18f505b7808e3967067c03a52f8cd44
Lucas Giudice <lucas.giudice@gmail.com>
2012-05-16 23:29:01 -0300

    probando el git... a ver que onda. Ya esta algo de rangos y de comportamiento de la agenda. Tambien hay un test :D

--- src/main/java/ar/edu/utn/tadp/agenda/Rango.java
+++ src/main/java/ar/edu/utn/tadp/agenda/Rango.java
@@ -1,25 +1,61 @@
 package ar.edu.utn.tadp.agenda;
 
-import org.joda.time.LocalDateTime;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
 
 public class Rango {
-	private LocalDateTime start;
-	private LocalDateTime end;
+	private DateTime start;
+	private DateTime end;
 	
-	public boolean includes(LocalDateTime aDate) {
-		return (this.start.isBefore(aDate)) && (this.end.isAfter(aDate));
+	public Rango(DateTime inicio, DateTime fin) {
+		this.setStart(inicio);
+		this.setEnd(fin);
+	}
+	
+	
+	/*
+	 * accesors
+	 */
+	
+	public DateTime getStart() {
+		return start;
+	}
+	
+	public void setStart(DateTime start) {
+		this.start = start;
+	}
+	
+	public DateTime getEnd() {
+		return end;
+	}
+	
+	public void setEnd(DateTime end) {
+		this.end = end;
+	}
+	
+	
+	/*
+	 * Methods
+	 */
+	
+	public boolean includes(DateTime aDate) {
+		return (this.getStart().isBefore(aDate)) && (this.getEnd().isAfter(aDate));
 	}
 
 	public boolean includes(Rango aRange) {
-		return (this.includes(aRange.start)) && (this.includes(aRange.end));
+		return (this.includes(aRange.getStart())) && (this.includes(aRange.getEnd()));
 	}
 
 	public boolean overlaps(Rango unRango) {
-		return this.includes(unRango.start) || this.includes(unRango.end);
+		return this.includes(unRango.getStart()) || this.includes(unRango.getEnd());
 	}
 
 	public boolean equals(Rango unRango) {
-		return start.equals(unRango.start) && end.equals(unRango.end);
+		return getStart().equals(unRango.getStart()) && getEnd().equals(unRango.getEnd());
+	}
+	public Period size(){
+		return new Period (this.getStart(), this.getEnd());
+		
 	}
 
 }
