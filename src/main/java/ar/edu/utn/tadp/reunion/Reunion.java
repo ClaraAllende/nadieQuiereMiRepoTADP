5bd1813cb91218e3db170bdc2e3e18a451ef0cd0
jvillarejo <arzivian87@gmail.com>
2012-05-18 19:16:18 -0300

    integracion reunion costeables

--- src/main/java/ar/edu/utn/tadp/reunion/Reunion.java
+++ src/main/java/ar/edu/utn/tadp/reunion/Reunion.java
@@ -1,5 +1,11 @@
 package ar.edu.utn.tadp.reunion;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import ar.edu.utn.tadp.costos.Costeable;
+
 public class Reunion {
 
 	public long getCantidadDePersonasQueNecesitanTransporte() {
@@ -8,5 +14,20 @@ public class Reunion {
 
 	public long getDuracionDeReunion() {
 		return 0;
+	}
+	
+	public BigDecimal getCostoTotal() {
+		BigDecimal result = BigDecimal.valueOf(0);
+		
+		for (Costeable costeable : dameListDeLosCosteables())
+		{
+			result.add(costeable.dameTuCostoPara(this));
+		}
+		
+		return result;
+	}
+
+	private Collection<Costeable> dameListDeLosCosteables() {
+		return new ArrayList<Costeable>();
 	}
 }
