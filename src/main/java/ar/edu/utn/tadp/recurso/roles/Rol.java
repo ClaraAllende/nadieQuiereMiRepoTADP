42bddf78e5bb0dfe7dbfa7e91e60941580666724
Paul <sancuba@gmail.com>
2012-05-19 02:03:43 -0300

    Se hizo el  2do test, se modifico Rol

--- src/main/java/ar/edu/utn/tadp/recurso/roles/Rol.java
+++ src/main/java/ar/edu/utn/tadp/recurso/roles/Rol.java
@@ -5,12 +5,19 @@ import java.math.BigDecimal;
 import ar.edu.utn.tadp.costos.CostoPorHora;
 
 public class Rol {
+	public static final Rol ARQUITECTO = new Rol(new CostoPorHora(
+			BigDecimal.valueOf(25.0)));
+	public static final Rol DIS_GRAFICO = new Rol(new CostoPorHora(
+			BigDecimal.valueOf(29.0)));
+	public static final Rol DIS_SISTEMAS = new Rol(new CostoPorHora(
+			BigDecimal.valueOf(30.0)));
+	public static final Rol PROYECT_LIDER = new Rol(new CostoPorHora(
+			BigDecimal.valueOf(35.0)));
+	public static final Rol GERENTE = new Rol(new CostoPorHora(
+			BigDecimal.valueOf(50.0)));
+	public static final Rol PROGRAMADOR = new Rol(new CostoPorHora(
+			BigDecimal.valueOf(150.0)));
 
-	public static final Rol DISENIADOR = new Rol(new CostoPorHora(BigDecimal.valueOf(25.0)));
-	public static final Rol LIDER = new Rol(new CostoPorHora(BigDecimal.valueOf(35.0)));
-	public static final Rol GERENTE = new Rol(new CostoPorHora(BigDecimal.valueOf(50.0)));
-	public static final Rol DESARROLLADOR = new Rol(new CostoPorHora(BigDecimal.valueOf(150.0)));
-	
 	private CostoPorHora costoPorHora;
 
 	public Rol(CostoPorHora costoPorHora) {
@@ -20,5 +27,4 @@ public class Rol {
 	public CostoPorHora getCostoPorHora() {
 		return this.costoPorHora;
 	}
-
 }
