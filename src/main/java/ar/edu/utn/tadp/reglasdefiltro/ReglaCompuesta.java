cc1c71979c2f623eb31ea974ed16a61a048e5bda
sancuba <BV@.(none)>
2012-05-30 08:53:10 -0300

    Tuve que limpiar un include de la clase ReglaCompuesta.java para que ande.

--- src/main/java/ar/edu/utn/tadp/reglasdefiltro/ReglaCompuesta.java
+++ src/main/java/ar/edu/utn/tadp/reglasdefiltro/ReglaCompuesta.java
@@ -4,16 +4,13 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import com.google.common.collect.Collections2;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
-
 import ar.edu.utn.tadp.recurso.Persona;
 
 public class ReglaCompuesta implements ReglaDeFiltrado {
 
-	private List<ReglaDeFiltrado> listaDeReglas;
+	private final List<ReglaDeFiltrado> listaDeReglas;
 
-	public ReglaCompuesta(List<ReglaDeFiltrado> listaDeReglas) {
+	public ReglaCompuesta(final List<ReglaDeFiltrado> listaDeReglas) {
 		this.listaDeReglas = listaDeReglas;
 	}
 
@@ -21,7 +18,7 @@ public class ReglaCompuesta implements ReglaDeFiltrado {
 	public Collection<Persona> filtrar(final Collection<Persona> personas) {
 		Collection<Persona> personasFiltradas = personas;
 		
-		for (ReglaDeFiltrado reglaDeFiltrado : listaDeReglas) {
+		for (final ReglaDeFiltrado reglaDeFiltrado : listaDeReglas) {
 			if(personasFiltradas.size() == 1) {
 				return personasFiltradas;
 			} else {
